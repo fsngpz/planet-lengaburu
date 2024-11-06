d6ab1a4f179c053be81d59d04507f93968c9033f
fsngpz <fsangap18@gmail.com>
2024-11-06 22:23:30 +0700

    add enum gender and relationship

---
+++ src/main/kotlin/Gender.kt
@@ -0,0 +1,10 @@
+/**
+ * The enumeration represent the gender.
+ *
+ * @author Ferdinand Sangap.
+ * @since 2024-11-06
+ */
+enum class Gender {
+    MALE,
+    FEMALE
+}
---
+++ src/main/kotlin/Relationship.kt
@@ -0,0 +1,17 @@
+/**
+ * The enumeration of relationship.
+ *
+ * @author Ferdinand Sangap.
+ * @since 2024-11-06
+ */
+enum class Relationship {
+    SON,
+    DAUGHTER,
+    SIBLINGS,
+    PATERNAL_UNCLE,
+    MATERNAL_UNCLE,
+    PATERNAL_AUNT,
+    MATERNAL_AUNT,
+    SISTER_IN_LAW,
+    BROTHER_IN_LAW
+}

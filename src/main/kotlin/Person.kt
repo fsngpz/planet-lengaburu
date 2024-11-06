d42af3fddc81e1d6babd5b15d3b36ad6f59fb306
fsngpz <fsangap18@gmail.com>
2024-11-06 22:23:35 +0700

    setup new data class family and person

---
+++ src/main/kotlin/Person.kt
@@ -0,0 +1,10 @@
+/**
+ * The class represent the entity of Person.
+ *
+ * @author Ferdinand Sangap.
+ * @since 2024-11-06
+ */
+data class Person(
+    val name: String,
+    val isChildOfParent: Boolean = false
+)
